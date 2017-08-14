1d5382e891e2768f8bd0395bbcaa5949dfba8155
Ella Jennings <ella.jennings@codecomputerlove.com>
2017-08-14 20:12:05 +0100

    added enumerator - good or bad guy

--- Warrior wars/Warrior wars/Warrior.cs
+++ Warrior wars/Warrior wars/Warrior.cs
@@ -5,11 +5,18 @@ namespace Warrior_wars
 {
     class Warrior
     {
+        private int goodGuyStartingHealth;
+        private int badGuyStartingHealth;
+
+        private Faction faction;
+
         private int _health;
         private string _name;
         private bool _isAlive;
 
         private Weapon weapon;
+        private Armor armor;
     }
+
 }
 
